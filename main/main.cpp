dc32f1e38233c7660d29d91036b462415862f544
Cristhian93-lang <a01668790@tec.mx>
2026-09-23 21:14:36 -0600

    Arreglo salida 0 0 en parte 3 y ajuste de estilo segun estandar

--- main/main.cpp
+++ main/main.cpp
@@ -1,270 +1,228 @@
-/*Descripcion: Programa que analiza archivos de transmision utilizando
-los algoritmos KMP, Manacher y programacion dinamica.
+/*
+ * Actividad Integradora 1
+ *
+ * Descripcion: Programa que analiza archivos de transmision utilizando
+ * los algoritmos KMP, Manacher y programacion dinamica.
+ * 
+ * Autores
+ * Isabella Montiel Reyes | A01278286
+ * Gerardo Martinez Carbajal | A01713474
+ * Cristhian Viery Maida Suarez | A01668790
+ *
+ * Compilacion para debug:
+ *   clang++ -std=c++17 -Wall -g *.cpp -o main
+ *
+ * Compilacion para ejecucion:
+ *   clang++ -std=c++17 -O3 *.cpp -o main
+ *
+ * Ejecucion:
+ *   ./main
+ */
 
-Autores: 
-- Isabella Montiel - A01278286
-- Gerardo Martínez Carbajal - A01713474
-- Cristhian Viery Maida Suarez - A01668790
-
-Fecha de modificacion: 23/09/2026. */
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <utility>
+#include <vector>
 #include <algorithm>
 
-using namespace std;
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::min;
+using std::pair;
+using std::string;
+using std::vector;
 
-/*Lee el contenido de un archivo e ignora los saltos de linea.
-Recibe el nombre del archivo que se desea leer.
-Retorna una cadena con el contenido del archivo.
-Complejidad: O(n).*/
-string leerArchivo(const string& nombreArchivo) {
+
+// Junta todo el archivo en una sola cadena y se salta los saltos de linea.
+// Si el archivo no existe avisa por cerr y regresa la cadena vacia.
+// Complejidad: O(n)
+string leerSinSaltos(const string& nombreArchivo) {
     ifstream archivo(nombreArchivo);
     string contenido = "";
     char caracter = '\0';
-
-    if (!archivo.is_open()) {
-        cerr << "Error al abrir " << nombreArchivo << endl;
-        return "";
-    }
-
-    while (archivo.get(caracter)) {
-        if (caracter != '\n' && caracter != '\r') {
-            contenido += caracter;
+    if (archivo.is_open()) {
+        while (archivo.get(caracter)) {
+            if (caracter != '\n' && caracter != '\r') {
+                contenido += caracter;
+            }
         }
+        archivo.close();
+    } else {
+        cerr << "No se encontro el archivo " << nombreArchivo << endl;
     }
-
-    archivo.close();
     return contenido;
 }
 
-// PARTE 1: Busqueda de codigos maliciosos con KMP.
-
-/*Construye el arreglo LPS utilizado por el algoritmo KMP.
-Recibe el patron que se desea buscar y el arreglo donde se
-almacenan las longitudes de prefijos y sufijos.
-No retorna ningun valor.
-Complejidad: O(m).*/
-void construirLPS(const string& patron, int lps[]) {
-    int longitud = 0;
+// Parte 1: busqueda del codigo malicioso con KMP
+ 
+// Para cada posicion del patron guarda el largo del prefijo que tambien
+// termina ahi como sufijo. Es lo que le dice a KMP a donde regresar.
+// Complejidad: O(m)
+vector<int> armarTablaLPS(const string &patron) {
+    int m = static_cast<int>(patron.length());
+    vector<int> lps(m, 0);
+    int largo = 0;
     int i = 1;
-
-    lps[0] = 0;
-
-    while (i < static_cast<int>(patron.length())) {
-        if (patron[i] == patron[longitud]) {
-            longitud++;
-            lps[i] = longitud;
+    while (i < m) {
+        if (patron[i] == patron[largo]) {
+            largo++;
+            lps[i] = largo;
+            i++;
+        } else if (largo != 0) {
+            largo = lps[largo - 1];
+        } else {
+            lps[i] = 0;
             i++;
         }
-        else {
-            if (longitud != 0) {
-                longitud = lps[longitud - 1];
-            }
-            else {
-                lps[i] = 0;
-                i++;
-            }
-        }
     }
+    return lps;
 }
-
-/*Busca la primera aparicion de un patron dentro de un texto utilizando KMP.
-Recibe el texto donde se realiza la busqueda y el patron que se desea buscar.
-Retorna la posicion inicial comenzando en 1 o -1 si el patron no se encuentra.
-Complejidad: O(n + m).*/
-int buscarKMP(const string& texto, const string& patron) {
-    if (patron.empty()) {
-        return -1;
-    }
-
-    int* lps = new int[patron.length()];
-    construirLPS(patron, lps);
-
-    int i = 0;
-    int j = 0;
-
-    while (i < static_cast<int>(texto.length())) {
-        if (texto[i] == patron[j]) {
-            i++;
-            j++;
-        }
-
-        if (j == static_cast<int>(patron.length())) {
-            int posicion = i - j;
-            delete[] lps;
-            return posicion + 1;
-        }
-
-        if (i < static_cast<int>(texto.length()) &&
-            texto[i] != patron[j]) {
-            if (j != 0) {
+ 
+// Busca la primera vez que aparece el codigo en la transmision. Regresa la
+// posicion donde empieza contando desde 1, o -1 si no aparece.
+// Complejidad: O(n + m)
+int localizarCodigo(const string &transmision, const string &codigo) {
+    int n = static_cast<int>(transmision.length());
+    int m = static_cast<int>(codigo.length());
+    int posicion = -1;
+    if (m > 0) {
+        vector<int> lps = armarTablaLPS(codigo);
+        int i = 0;
+        int j = 0;
+        while (i < n && posicion == -1) {
+            if (transmision[i] == codigo[j]) {
+                i++;
+                j++;
+                if (j == m) {
+                    posicion = i - m + 1;
+                }
+            } else if (j != 0) {
                 j = lps[j - 1];
-            }
-            else {
+            } else {
                 i++;
             }
         }
     }
-
-    delete[] lps;
-    return -1;
+    return posicion;
 }
-
-/*Busca un codigo dentro de una transmision y muestra el resultado.
-Recibe la transmision donde se realiza la busqueda y el codigo a buscar.
-No retorna ningun valor.
-Complejidad: O(n + m).*/
-void mostrarBusqueda(const string& transmision, const string& codigo) {
-    int posicion = buscarKMP(transmision, codigo);
-
+ 
+// Imprime "true" con la posicion si el codigo esta en la transmision, o "false".
+// Complejidad: O(n + m)
+void reportarCodigo(const string &transmision, const string &codigo) {
+    int posicion = localizarCodigo(transmision, codigo);
     if (posicion != -1) {
         cout << "true " << posicion << endl;
-    }
-    else {
+    } else {
         cout << "false" << endl;
     }
 }
-
-// PARTE 2: Palindromo mas largo con Manacher.
-
-/*Encuentra el palindromo mas largo utilizando el algoritmo de Manacher.
-Recibe el texto en el que se desea buscar el palindromo.
-Retorna un par con las posiciones inicial y final comenzando en 1.
-Complejidad: O(n).*/
-pair<int, int> palindromoMasLargo(const string& texto) {
-    if (texto.empty()) {
-        return {0, 0};
+ 
+// Parte 2: codigo espejeado mas largo con Manacher
+ 
+// Mete un separador entre cada caracter para que los palindromos pares e
+// impares se traten igual, y aprovecha la posicion espejo para no volver a
+// comparar lo que ya se sabe. Regresa inicio y fin contando desde 1.
+// Complejidad: O(n)
+pair<int, int> ubicarEspejeado(const string &texto) {
+    pair<int, int> resultado(0, 0);
+    if (!texto.empty()) {
+        string separado = "^";
+        for (char caracter : texto) {
+            separado += "#";
+            separado += caracter;
+        }
+        separado += "#$";
+        int n = static_cast<int>(separado.length());
+        vector<int> radios(n, 0);
+        int centro = 0;
+        int derecha = 0;
+        int mejorCentro = 0;
+        int mejorRadio = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (i < derecha) {
+                radios[i] = min(derecha - i, radios[2 * centro - i]);
+            }
+            while (separado[i + 1 + radios[i]] == separado[i - 1 - radios[i]]) {
+                radios[i]++;
+            }
+            if (i + radios[i] > derecha) {
+                centro = i;
+                derecha = i + radios[i];
+            }
+            if (radios[i] > mejorRadio) {
+                mejorRadio = radios[i];
+                mejorCentro = i;
+            }
+        }
+        int inicio = (mejorCentro - mejorRadio) / 2;
+        resultado.first = inicio + 1;
+        resultado.second = inicio + mejorRadio;
     }
-
-    string transformado = "^";
-
-    for (char caracter : texto) {
-        transformado += "#";
-        transformado += caracter;
-    }
-
-    transformado += "#$";
-
-    int n = static_cast<int>(transformado.length());
-    int* radios = new int[n]();
-
-    int centro = 0;
-    int derecha = 0;
-    int mejorCentro = 0;
-    int mejorLongitud = 0;
-
-    for (int i = 1; i < n - 1; i++) {
-        int espejo = 2 * centro - i;
-
-        if (i < derecha) {
-            radios[i] = min(derecha - i, radios[espejo]);
-        }
-
-        while (transformado[i + 1 + radios[i]] ==
-               transformado[i - 1 - radios[i]]) {
-            radios[i]++;
-        }
-
-        if (i + radios[i] > derecha) {
-            centro = i;
-            derecha = i + radios[i];
-        }
-
-        if (radios[i] > mejorLongitud) {
-            mejorLongitud = radios[i];
-            mejorCentro = i;
-        }
-    }
-
-    int inicio = (mejorCentro - mejorLongitud) / 2;
-    int fin = inicio + mejorLongitud - 1;
-
-    delete[] radios;
-
-    return {inicio + 1, fin + 1};
+    return resultado;
 }
-
-// PARTE 3: Substring comun mas largo.
-
-/*Encuentra el substring comun mas largo utilizando programacion dinamica.
-Recibe los dos textos que se desean comparar y las variables donde se
-guardan las posiciones inicial y final del substring en el primer texto.
-No retorna ningun valor.
-Complejidad: O(n * m) en tiempo y O(m) en memoria.*/
-void substringComunMasLargo(const string& texto1, const string& texto2,
-    int& inicio, int& fin) {
+ 
+// Parte 3: substring comun mas largo con programacion dinamica
+ 
+// En lugar de la matriz completa solo se guardan dos filas que se van
+// intercambiando. Regresa inicio y fin dentro de la primera transmision,
+// o 0 0 si no comparten ningun caracter.
+// Complejidad: O(n * m) en tiempo y O(m) en memoria
+pair<int, int> ubicarTramoComun(const string &texto1, const string &texto2) {
     int n = static_cast<int>(texto1.length());
     int m = static_cast<int>(texto2.length());
-
-    int* anterior = new int[m + 1]();
-    int* actual = new int[m + 1]();
-
-    int longitudMaxima = 0;
-    int posicionFinal = 0;
-
+    vector<int> anterior(m + 1, 0);
+    vector<int> actual(m + 1, 0);
+    int largoMaximo = 0;
+    int finEnTexto1 = 0;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             if (texto1[i - 1] == texto2[j - 1]) {
                 actual[j] = anterior[j - 1] + 1;
-
-                if (actual[j] > longitudMaxima) {
-                    longitudMaxima = actual[j];
-                    posicionFinal = i;
+                if (actual[j] > largoMaximo) {
+                    largoMaximo = actual[j];
+                    finEnTexto1 = i;
                 }
-            }
-            else {
+            } else {
                 actual[j] = 0;
             }
         }
-
-        int* temporal = anterior;
-        anterior = actual;
-        actual = temporal;
-
-        for (int j = 0; j <= m; j++) {
-            actual[j] = 0;
-        }
+        anterior.swap(actual);
     }
-
-    inicio = posicionFinal - longitudMaxima + 1;
-    fin = posicionFinal;
-
-    delete[] anterior;
-    delete[] actual;
+    pair<int, int> resultado(0, 0);
+    if (largoMaximo > 0) {
+        resultado.first = finEnTexto1 - largoMaximo + 1;
+        resultado.second = finEnTexto1;
+    }
+    return resultado;
 }
-
-/*Ejecuta las tres partes de la actividad utilizando los cinco archivos
-de entrada y muestra los resultados requeridos.
-No recibe parametros.
-Retorna 0 cuando el programa termina correctamente.*/
+ 
+// Carga los cinco archivos y saca las tres partes en el orden que pide la actividad.
+// Complejidad: O(n * m), la tercera parte es la que domina
 int main() {
-    string transmission1 = leerArchivo("transmission1.txt");
-    string transmission2 = leerArchivo("transmission2.txt");
-    string mcode1 = leerArchivo("mcode1.txt");
-    string mcode2 = leerArchivo("mcode2.txt");
-    string mcode3 = leerArchivo("mcode3.txt");
-
-    mostrarBusqueda(transmission1, mcode1);
-    mostrarBusqueda(transmission1, mcode2);
-    mostrarBusqueda(transmission1, mcode3);
-    mostrarBusqueda(transmission2, mcode1);
-    mostrarBusqueda(transmission2, mcode2);
-    mostrarBusqueda(transmission2, mcode3);
-
-    pair<int, int> palindromo1 = palindromoMasLargo(transmission1);
-    pair<int, int> palindromo2 = palindromoMasLargo(transmission2);
-
-    cout << palindromo1.first << " " << palindromo1.second << endl;
-    cout << palindromo2.first << " " << palindromo2.second << endl;
-
-    int inicio = 0;
-    int fin = 0;
-
-    substringComunMasLargo(transmission1, transmission2, inicio, fin);
-    cout << inicio << " " << fin << endl;
-
+    string transmision1 = leerSinSaltos("transmission1.txt");
+    string transmision2 = leerSinSaltos("transmission2.txt");
+    string codigo1 = leerSinSaltos("mcode1.txt");
+    string codigo2 = leerSinSaltos("mcode2.txt");
+    string codigo3 = leerSinSaltos("mcode3.txt");
+ 
+    reportarCodigo(transmision1, codigo1);
+    reportarCodigo(transmision1, codigo2);
+    reportarCodigo(transmision1, codigo3);
+    reportarCodigo(transmision2, codigo1);
+    reportarCodigo(transmision2, codigo2);
+    reportarCodigo(transmision2, codigo3);
+ 
+    pair<int, int> espejo1 = ubicarEspejeado(transmision1);
+    pair<int, int> espejo2 = ubicarEspejeado(transmision2);
+    cout << espejo1.first << " " << espejo1.second << endl;
+    cout << espejo2.first << " " << espejo2.second << endl;
+ 
+    pair<int, int> tramo = ubicarTramoComun(transmision1, transmision2);
+    cout << tramo.first << " " << tramo.second << endl;
+ 
     return 0;
 }
